6a73322093c2dfe2cf085853e925187c301f005a
kyletho <k.hecker92@gmail.com>
2025-12-19 15:26:27 -0500

    major changes to gameboard

--- tictactoe.js
+++ tictactoe.js
@@ -10,10 +10,33 @@ function GameBoard() {
             board[i][j] = Square();
         }
     }
+
+    const printBoard = () => {
+        console.log("=BOARD=");
+        console.log(`[${board[0][0].getValue()}] [${board[0][1].getValue()}] [${board[0][2].getValue()}]`);
+        console.log("-----------");
+        console.log(`[${board[1][0].getValue()}] [${board[1][1].getValue()}] [${board[1][2].getValue()}]`);
+        console.log("-----------");
+        console.log(`[${board[2][0].getValue()}] [${board[2][1].getValue()}] [${board[2][2].getValue()}]`);
+    };
+
+    const getBoard = () => board();
+
+    return { getBoard, printBoard };
     
 }
 
 function Square () {
+
+    let value = "";
+
+    const addToken = (playerShape) => {
+        value = playerShape;
+    };
+
+    const getValue = () => value || " ";
+
+    return { getValue, addToken }
     
 }
 
@@ -25,3 +48,6 @@ function Player(shape) {
 function GameState() {
     
 }
+
+const game = GameBoard();
+game.printBoard();
